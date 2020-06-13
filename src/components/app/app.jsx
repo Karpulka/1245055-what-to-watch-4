08e86c90bdaef56367c09ff43056061d73398a53
Alyona Karpova <karpova.dev@yandex.ru>
2020-06-13 19:30:55 +0300

    создан пропс с названиями фильмов и передан в компоненты, добавлен компонент Film отвечающий за рендеринг одной карточки фильма

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import {Main} from "../main/main.jsx";
 
 export const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {promoFilmSettings} = props;
-  // eslint-disable-next-line react/prop-types
-  return <Main promoFilmTitle={promoFilmSettings.title} promoFilmGenre={promoFilmSettings.genre} pormoFilmYear={promoFilmSettings.year}/>;
+  const {settings: {promoFilm: promoFilmSettings, films}} = props;
+
+  return <Main
+    promoFilmTitle={promoFilmSettings.title}
+    promoFilmGenre={promoFilmSettings.genre}
+    pormoFilmYear={promoFilmSettings.year}
+    films={films}
+  />;
 };
--- src/index.js
+++ src/index.js
@@ -3,14 +3,42 @@ import ReactDOM from "react-dom";
 import {App} from "./components/app/app.jsx";
 
 const init = () => {
-  const promoFilmSettings = {
+  const promoFilm = {
     title: `The Grand Budapest Hotel`,
     genre: `Drama`,
     year: 2014
   };
 
+  const films = [
+    `Fantastic Beasts: The Crimes of Grindelwald`,
+    `Bohemian Rhapsody`,
+    `Macbeth`,
+    `Aviator`,
+    `We need to talk about Kevin`,
+    `What We Do in the Shadows`,
+    `Revenant`,
+    `Johnny English`,
+    `Shutter Island`,
+    `Pulp Fiction`,
+    `No Country for Old Men`,
+    `Snatch`,
+    `Moonrise Kingdom`,
+    `Seven Years in Tibet`,
+    `Midnight Special`,
+    `War of the Worlds`,
+    `Dardjeeling Limited`,
+    `Orlando`,
+    `Mindhunter`,
+    `Midnight Special`
+  ];
+
+  const settings = {
+    promoFilm,
+    films
+  };
+
   ReactDOM.render(
-      <App promoFilmSettings={promoFilmSettings}/>,
+      <App settings={settings}/>,
       document.querySelector(`#root`)
   );
 };
