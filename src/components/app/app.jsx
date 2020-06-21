e86df6a014dc5915f4176d8317a4495d3c174cf5
Alyona Karpova <karpova.dev@yandex.ru>
2020-06-21 22:41:02 +0300

    подключен react-router-dom, создан компонент FilmDetail

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import Main from "../main/main.jsx";
 import PropTypes from "prop-types";
+import FilmDetail from "../film-detail/film-detail.jsx";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 const App = (props) => {
   const {settings: {promoFilm: promoFilmSettings, films}} = props;
 
-  return <Main
-    promoFilmTitle={promoFilmSettings.title}
-    promoFilmGenre={promoFilmSettings.genre}
-    promoFilmYear={promoFilmSettings.year}
-    films={films}
-  />;
+  return <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <Main
+          promoFilmTitle={promoFilmSettings.title}
+          promoFilmGenre={promoFilmSettings.genre}
+          promoFilmYear={promoFilmSettings.year}
+          films={films}
+        />
+      </Route>
+      <Route exact path="/film-detail">
+        <FilmDetail/>
+      </Route>
+    </Switch>
+  </BrowserRouter>;
 };
 
 App.propTypes = {
